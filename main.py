618ed47941bb32212df8d159604f820aa4b839dc
ahmetpala <ahmtpala13@gmail.com>
2025-04-18 20:22:24 +0200

    Define plot functions in utils

--- main.py
+++ main.py
@@ -2,37 +2,11 @@ import argparse
 import os
 from pathlib import Path
 
-import matplotlib.pyplot as plt
 from keras.datasets import mnist
 
 from generator import generate_samples
 from model import build_vae
-from utils import load_config, plot_sample_images
-
-
-def plot_loss(history, save_path):
-    """Plot training and validation loss curves."""
-    plt.plot(history['loss'], label="Train loss")
-    plt.plot(history['val_loss'], label="Validation loss")
-    plt.xlabel("Epoch")
-    plt.ylabel("Loss")
-    plt.title("Loss change over epochs")
-    plt.legend()
-    plt.savefig(save_path)
-    plt.close()
-
-
-def plot_latent_space(mu, y_test, save_path):
-    """Plot 2D latent space."""
-    # TODO: Check if mu is high dimensional (>2), if so, apply PCA
-    plt.figure(figsize=(12, 8))
-    plt.scatter(mu[:, 0], mu[:, 1], c=y_test, cmap='brg')
-    plt.xlabel('mu[:, 0]')
-    plt.ylabel('mu[:, 1]')
-    plt.title("Latent space visualization")
-    plt.colorbar()
-    plt.savefig(save_path)
-    plt.close()
+from utils import load_config, plot_latent_space, plot_loss, plot_sample_images
 
 
 def main():
